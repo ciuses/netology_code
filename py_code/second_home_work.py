b61e205d0cb2b58c38fe5374b3ec659eb62ebc14
ciuse <ciuse@yandex.ru>
2022-10-20 21:41:56 +0700

    ипотека 2

--- py_code/second_home_work.py
+++ py_code/second_home_work.py
@@ -3,6 +3,10 @@ base_rate = 10
 far_east = ['анадырь', 'благовещенск', 'биробиджан', 'владивосток', 'комсомольск-на-амуре', 'магадан',
             'петропавловск-камчатский', 'хабаровск', 'южно-сахалинск', 'якутск']
 
+children_bonus = 0
+salary_bonus = 0
+insurance_bonus = 0
+
 city = input('Ввведите Ваш город регистрации:(например Хабаровск)\n').lower()
 amount_of_children = int(input('Укажите количество детей? (например 2)\n'))
 salary_project = input('Являетесь ли Вы нашим клиентом по зарплатному проекту? (да\нет)\n').lower()
@@ -15,39 +19,40 @@ if city in far_east:
 else:
 
     if amount_of_children > 3:
-        base_rate -= 1
+        children_bonus = 1
 
         if salary_project == 'да':
-            base_rate -= 0.5
+            salary_bonus = 0.5
 
             if insurance == 'да':
-                base_rate -= 1.5
-                print(f'Ваша базовая ставка по ипотеке будет {base_rate}%')
+                insurance_bonus = 1.5
+                print(
+                    f'Ваша базовая ставка по ипотеке будет {base_rate - children_bonus - salary_bonus - insurance_bonus}%')
 
             else:
-                print(f'Ваша базовая ставка по ипотеке будет {base_rate}%')
+                print(f'Ваша базовая ставка по ипотеке будет {base_rate - children_bonus - salary_bonus}%')
 
         else:
-            print(f'Ваша базовая ставка по ипотеке будет {base_rate}%')
+            print(f'Ваша базовая ставка по ипотеке будет {base_rate - children_bonus}%')
 
 
     else:
 
         if salary_project == 'да':
-            base_rate -= 0.5
+            salary_bonus = 0.5
 
             if insurance == 'да':
-                base_rate -= 1.5
-                print(f'Ваша базовая ставка по ипотеке будет {base_rate}%')
+                insurance_bonus = 1.5
+                print(f'Ваша базовая ставка по ипотеке будет {base_rate - salary_bonus - insurance_bonus}%')
 
             else:
-                print(f'Ваша базовая ставка по ипотеке будет {base_rate}%')
+                print(f'Ваша базовая ставка по ипотеке будет {base_rate - salary_bonus}%')
 
         else:
 
             if insurance == 'да':
-                base_rate -= 1.5
-                print(f'Ваша базовая ставка по ипотеке будет {base_rate}%')
+                insurance_bonus = 1.5
+                print(f'Ваша базовая ставка по ипотеке будет {base_rate - insurance_bonus}%')
 
             else:
                 print(f'Ваша базовая ставка по ипотеке будет {base_rate}%')
