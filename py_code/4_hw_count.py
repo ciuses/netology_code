8dd9a0f803649fd1a1b4e5faeb5e5b0e67410b73
ciuse <ciuse@yandex.ru>
2022-10-30 19:21:29 +0700

    исправил 3ю домашку

--- py_code/4_hw_count.py
+++ py_code/4_hw_count.py
@@ -8,19 +8,35 @@ queries = [
     'сериалы про спорт'
 ]
 
-three, two, one = 0, 0, 0
+# three, two, one = 0, 0, 0
+#
+# for substring in queries:
+#     list_words = substring.split()
+#     if len(list_words) == 3:
+#         three += 1
+#     elif len(list_words) == 2:
+#         two += 1
+#     elif len(list_words) == 2:
+#         one += 1
+#
+# print(
+#     f'Поисковые запросы из:\n'
+#     f'трёх слов составляют -> {three * 100 / len(queries):.2f}%\n'
+#     f'двух слов составляют -> {two * 100 / len(queries):.2f}%\n'
+#     f'одного слова составляют -> {one * 100 / len(queries):.2f}%')
 
-for substring in queries:
-    list_words = substring.split()
-    if len(list_words) == 3:
-        three += 1
-    elif len(list_words) == 2:
-        two += 1
-    elif len(list_words) == 2:
-        one += 1
+requests_count = {}
+
+for request in queries:
+    words_number = len(request.split())
+    if words_number in requests_count:
+        requests_count[words_number] += 1
+    else:
+        requests_count[words_number] = 1
+
+first_key, second_key = requests_count.keys()
 
 print(
     f'Поисковые запросы из:\n'
-    f'трёх слов составляют -> {three * 100 / len(queries):.2f}%\n'
-    f'двух слов составляют -> {two * 100 / len(queries):.2f}%\n'
-    f'одного слова составляют -> {one * 100 / len(queries):.2f}%')
+    f'{first_key} слов составляют -> {requests_count[3] * 100 / len(queries):.2f}%\n'
+    f'{second_key} слов составляют -> {requests_count[2] * 100 / len(queries):.2f}%\n')
