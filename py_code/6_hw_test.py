164fe13b7864335175f41b5325e4f5a5e3ffe13e
ciuse <ciuse@yandex.ru>
2022-11-06 14:32:29 +0700

    шестая домашка тест, некоторые куски кода

---
+++ py_code/6_hw_test.py
@@ -0,0 +1,34 @@
+class Income:
+    def __init__(self, id_):
+        self.id_ = id_
+        id_ = 100
+
+in_1 = Income(1000)
+print(in_1.id_)
+
+class Pers:
+    def __init__(self, id):
+        self.id = id
+
+some_pers = Pers(100)
+# some_pers.id = 100
+some_pers.__dict__['age'] = 30
+print(some_pers.age + len(some_pers.__dict__))
+
+class Income:
+    def __int__(self, id_):
+        self.id_ = id_
+
+    def test(self, word):
+        print(f'My word is {word}')
+
+    def test(self, int):
+        print(f'My ineger is {int}')
+
+
+my_income = Income()
+print(my_income.test('test'))
+
+# print(my_income.isinstans(Income))
+# print(Income.isinstans(my_income))
+print(isinstance(my_income, Income))
