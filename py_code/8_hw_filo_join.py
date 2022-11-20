0ee98b11732d7cfeb8c43c3d8b20af3d4cc19b6f
ciuse <ciuse@yandex.ru>
2022-11-20 16:10:32 +0700

    оно работает но первая строка с некрасивым пееводом в новую стоку

--- py_code/8_hw_filo_join.py
+++ py_code/8_hw_filo_join.py
@@ -9,32 +9,21 @@ def prepare_your_objects(path_to_fils: str = 'other\\files\\') -> list:
     return sorted(row_list, key=lambda row_list: row_list[1])
 
 
-def write_your_objects(list_obj_and_len: str = prepare_your_objects()):
-    for filo, filo_len in list_obj_and_len:
-        filo_path = filo.name
-        _, _, filo_name = filo_path.split('\\')
-        filo.seek(0)
-        write_to_filo(filo_name, filo_len, filo.readlines())
-
-
-
 def write_to_filo(filo_name: str, filo_len: str, list_of_lines: list):
     with open('final_filo.txt', 'a', encoding='utf8') as file_object:
-        file_object.write(f'{filo_name}\n{filo_len}\n')
+        file_object.write(f'\n{filo_name}\n{filo_len}\n')
         for line in list_of_lines:
             file_object.write(line)
 
 
 
+def write_control(list_obj_and_len: str = prepare_your_objects()):
+    for filo, filo_len in list_obj_and_len:
+        filo_path = filo.name
+        _, _, filo_name = filo_path.split('\\')
+        filo.seek(0)
+        write_to_filo(filo_name, filo_len, filo.readlines())
+        filo.close()
+
 if __name__ == '__main__':
-
-
-    # for obj in prepare_your_objects():
-    #     print(obj)
-    #     sp_obj = obj[0]
-    #     print(sp_obj.readlines())
-
-
-    write_your_objects()
-
-    # write_to_filo('test.txt', '23')
+    write_control()
