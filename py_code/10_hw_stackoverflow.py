be5f6d53ed8e0756454386f72d7f84e80aa384ec
ciuse <ciuse@yandex.ru>
2022-12-07 15:50:41 +0700

    домашка № 3, готово

--- py_code/10_hw_stackoverflow.py
+++ py_code/10_hw_stackoverflow.py
@@ -1,16 +1,22 @@
-import json
 import requests
 
+def get_questions(tag: str) -> object:
+    link = 'https://api.stackexchange.com/2.3/questions'
+    params = {'fromdate': '1670284800', 'tagged': tag, 'site': 'stackoverflow'}
+    response = requests.get(link, params=params)
+    if response.status_code == 200:
+        my_dict = response.json()
+        questions_dict = {}
+        for all_info in my_dict['items']:
+            questions_dict[all_info['title']] = all_info['link'], all_info['tags']
+        return questions_dict
+    else:
+        return response.status_code
 
-link = 'https://api.stackexchange.com/2.3/questions'
-params = {'fromdate': '1670284800', 'tagged': 'python', 'site': 'stackoverflow'}
 
-response = requests.get(link, params=params)
-my_dict = response.json()
-questions_dict = {}
-for all_info in my_dict['items']:
-    questions_dict[all_info['title']] = all_info['link'], all_info['tags']
+if __name__ == '__main__':
 
+    for k, v in get_questions('python').items():
+        print(k, v)
 
-for k, v in questions_dict.items():
-    print(k, v)
+    # TODO Подачу даты времени ввиде эпохи Unix из 1670284800 в Tue Dec  6 07:00:00 2022 print(time.ctime(1670284800))
