f342d2a28426df01aad099a89c62c1acc357f20a
ciuse <ciuse@yandex.ru>
2023-01-10 13:04:30 +0700

    мелкие правки

---
+++ sql_code/del_table.sql
@@ -0,0 +1,8 @@
+DROP TABLE albums,
+	artists_albums,
+	collection,
+	genres_artists,
+	music_artists,
+	music_genres,
+	tracks
+CASCADE;
--- sql_code/код_по_созданию_таблиц_со_связями.sql
+++ sql_code/код_по_созданию_таблиц_со_связями.sql
@@ -3,21 +3,17 @@ CREATE TABLE IF NOT EXISTS genres (
 	genres_name VARCHAR(60) NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS artists (
 	id SERIAL PRIMARY KEY,
 	artists_name VARCHAR(60) NOT NULL
 );
 
-
-
 CREATE TABLE IF NOT EXISTS genres_artists (
 	genres_id INTEGER REFERENCES genres(id),
 	artists_id INTEGER REFERENCES artists(id),
-	CONSTRAINT pk_genres_artists PRIMARY KEY (genres_id, artists_id)
+	CONSTRAINT pk_genres_artists PRIMARY KEY (genres_id,
+artists_id)
 );
-
-
 
 CREATE TABLE IF NOT EXISTS albums (
 	id SERIAL PRIMARY KEY,
@@ -25,14 +21,12 @@ CREATE TABLE IF NOT EXISTS albums (
 	album_data INTEGER
 );
 
-
-
 CREATE TABLE IF NOT EXISTS artists_albums (
 	artists_id INTEGER REFERENCES artists(id),
 	album_id INTEGER REFERENCES albums(id),
-	CONSTRAINT pk_artists_albums PRIMARY KEY (artists_id, album_id)
+	CONSTRAINT pk_artists_albums PRIMARY KEY (artists_id,
+album_id)
 );
-
 
 CREATE TABLE IF NOT EXISTS tracks (
 	id SERIAL PRIMARY KEY,
@@ -41,16 +35,15 @@ CREATE TABLE IF NOT EXISTS tracks (
 	album_id INTEGER REFERENCES albums(id)
 );
 
-
 CREATE TABLE IF NOT EXISTS collection (
 	id SERIAL PRIMARY KEY,
 	сollection_name VARCHAR(80) NOT NULL,
 	сollection_date INTEGER
 );
 
-
 CREATE TABLE IF NOT EXISTS tracks_collection (
 	tracks_id INTEGER REFERENCES tracks(id),
 	collection_id INTEGER REFERENCES collection(id),
-	CONSTRAINT pk_tracks_collection PRIMARY KEY (tracks_id, collection_id)
+	CONSTRAINT pk_tracks_collection PRIMARY KEY (tracks_id,
+collection_id)
 );
