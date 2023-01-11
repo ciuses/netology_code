998cb54feda2d95b08114e4e7c2743ab4178aa6c
ciuse <ciuse@yandex.ru>
2023-01-11 14:44:51 +0700

    исправил шефа и добавил 2 файла по новому заданию

--- sql_code/Chief.sql
+++ sql_code/Chief.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS employees (
 	id SERIAL PRIMARY KEY,
 	employee VARCHAR(40) NOT NULL,
-	chief VARCHAR(40) UNIQUE,
-	department varchar(100)
+	chief INTEGER REFERENCES employees(id),
+	department VARCHAR(100)
 );
 
