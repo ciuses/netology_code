2c9cc73b9f4e5839ce24338530adb378cf376780
ciuse <ciuse@yandex.ru>
2023-01-12 14:32:39 +0700

    вставки данных

---
+++ sql_code/inserts.sql
@@ -0,0 +1,73 @@
+--INSERT INTO artists(artists_name)
+--VALUES('rammstein');
+
+--DELETE FROM artists;
+
+
+INSERT INTO artists
+VALUES (1, 'rammstein'),
+	(2, 'madonna'),
+	(3, 'the prodigy'),
+	(4, 'кино'),
+	(5, 'eminem'),
+	(6, 'алла пугачёва'),
+	(7, 'joe dassin'),
+	(8, 'иога́нн себастья́н бах');
+
+INSERT INTO genres
+VALUES (1, 'industrial metal'),
+	(2, 'pop'),
+	(3, 'electronic music'),
+	(4, 'rock'),
+	(5, 'rap'),
+	(6, 'chanson'),
+	(7, 'classical music');
+
+INSERT INTO albums
+VALUES (1, 'Mutter', 2019),
+	(2, 'Sehnsucht', 1997),
+	(3, 'Like a Virgin', 1984),
+	(4, 'Music', 2020),
+	(5, 'The Fat of the Land', 1997),
+	(6, 'Worlds on Fire', 2018),
+	(7, 'Звезда по имени Солнце', 1989),
+	(8, 'Ночь', 2018),
+	(9, 'The Eminem Show', 2002),
+	(10, 'The Slim Shady LP', 1999),
+	(11, 'Зеркало души', 1978),
+	(12, 'То ли ещё будет…', 2020),
+	(13, 'Les Champs-Elysees', 1969),
+	(14, 'Le Jardin du Luxembourg', 1976),
+	(15, 'The Greatest Hits', 2020),
+	(16, '100 Лучших Произведений', 2019);
+--
+--
+--INSERT INTO tracks(track_name, track_duration, album_id)
+--VALUES ('Mutter', 2019),
+--	('Sehnsucht', 1997),
+--	('Like a Virgin', 1984),
+--	('Music', 2020),
+--	('The Fat of the Land', 1997),
+--	('Worlds on Fire', 2018),
+--	('Звезда по имени Солнце', 1989),
+--	('Ночь', 2018),
+--	('The Eminem Show', 2002),
+--	('The Slim Shady LP', 1999),
+--	('Зеркало души', 1978),
+--	('То ли ещё будет…', 2020),
+--	('Les Champs-Elysees', 1969),
+--	('Le Jardin du Luxembourg', 1976),
+--	('The Greatest Hits', 2020),
+--	('100 Лучших Произведений', 2019);
+
+
+
+
+
+
+
+
+
+
+
+
