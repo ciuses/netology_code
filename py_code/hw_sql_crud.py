b67dcaa3c1c9f4592d345090e50f400431245a80
ciuse <ciuse@yandex.ru>
2023-01-17 12:45:23 +0700

    исправил функцию вставки и добавил функцию добавки номера

--- py_code/hw_sql_crud.py
+++ py_code/hw_sql_crud.py
@@ -28,6 +28,12 @@ import psycopg2
 #
 # conn.close()
 
+def drop_tables(connect: object, table_name: str):
+    '''Удаляет таблицы перданные в аргумете table_name.'''
+    with connect.cursor() as cursor:
+        cursor.execute(f"""DROP TABLE {table_name};""")
+    connect.commit()
+
 def create_tables(connect: object):
     '''Создаёт таблицеу clients и phone_numbers.'''
 
@@ -49,10 +55,48 @@ def create_tables(connect: object):
 
     connect.commit() # TODO дописать какие нить принты
 
-def drop_tables(connect: object, table_name: str):
-    '''Удаляет таблицы перданные в аргумете table_name.'''
+
+def add_new_client(connect: object, f_name: str, l_name: str, email: str, phone: str=None):
+    '''Добавляет клиента, можно без телофона'''
+
+    if phone == None:
+
+        with connect.cursor() as cursor:
+
+            cursor.execute("""
+                        INSERT INTO clients(first_name, last_name, email)
+                        VALUES (%s, %s, %s);""", (f_name, l_name, email))
+
+    else:
+
+        with connect.cursor() as cursor:
+
+            cursor.execute("""
+                        INSERT INTO clients(first_name, last_name, email)
+                        VALUES (%s, %s, %s) RETURNING id;
+                        """, (f_name, l_name, email))
+
+            id_for_link_phone = cursor.fetchone()[0]
+
+            cursor.execute("""
+                        INSERT INTO phone_numbers
+                        VALUES (%s, %s);
+                        """, (id_for_link_phone, phone))
+
+    connect.commit()
+
+
+def add_phone(connect: object, client_id: int, phone: str):
+    '''Создаёт запись в табле телефонов с айди существющего клента.'''
+
     with connect.cursor() as cursor:
-        cursor.execute(f"""DROP TABLE {table_name};""")
+
+        cursor.execute("""
+                    INSERT INTO phone_numbers
+                    VALUES (%s, %s);
+                    """, (client_id, phone))
+
+
     connect.commit()
 
 
@@ -64,10 +108,22 @@ if __name__ == '__main__':
                               user='postgres',
                               password='hanson')
 
-    create_tables(my_con)
 
     # drop_tables(my_con, 'phone_numbers')
     # drop_tables(my_con, 'clients')
+
+    # create_tables(my_con)
+
+    # add_new_client(my_con, 'Фёдор', 'Дядя', 'fedor@df_city.com')
+    # add_new_client(my_con, 'Матроскин', 'Кот', 'matroskin@df_city.com', '79029017776')
+    # add_new_client(my_con, 'Шарик', 'Пёс', 'sharick@df_city.com', '79039014862')
+    # add_new_client(my_con, 'Галчёнок', 'Кто Там','jackdaw@village.com', '79049019559')
+    # add_new_client(my_con, 'Игорь Иванович', 'Печкин', 'pechkin@village.com', '79049019559')
+
+    # add_phone(my_con, 16, '79019011111')
+    add_phone(my_con, 16, '79019013333')
+
+
 
     my_con.close()
 
